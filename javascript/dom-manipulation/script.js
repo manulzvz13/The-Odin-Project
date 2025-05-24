ed9cc7610be6c2200ff3cda35135cf20b11dd149
manulzvz13 <manulzvz@gmail.com>
2025-05-23 19:48:01 -0600

    feat: Add JS attributes to the exercise

--- javascript/dom-manipulation/script.js
+++ javascript/dom-manipulation/script.js
@@ -5,4 +5,12 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 container.appendChild(content);
 
+const para = document.createElement("p");
+para.textContent= "Hey I'm red!";
+para.style.color = "red";
+container.appendChild(para);
 
+const headThree = document.createElement("h3");
+headThree.textContent = "I'm blue h3!";
+headThree.style.color = "blue";
+container.appendChild(headThree);
