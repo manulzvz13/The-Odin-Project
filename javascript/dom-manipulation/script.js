dfc3c65c28a5e0d2d383c79feb94e8c41a0b5b40
manulzvz13 <manulzvz@gmail.com>
2025-05-24 13:23:32 -0600

    feat: Add childs to DOM via JS

--- javascript/dom-manipulation/script.js
+++ javascript/dom-manipulation/script.js
@@ -14,3 +14,14 @@ const headThree = document.createElement("h3");
 headThree.textContent = "I'm blue h3!";
 headThree.style.color = "blue";
 container.appendChild(headThree);
+
+const contentTwo = document.createElement("div");
+contentTwo.setAttribute("style", "border: solid black; background: pink;");
+const headOne = document.createElement("h1");
+headOne.textContent = "I'm in a div";
+const paraTwo = document.createElement("p");
+paraTwo.textContent = "ME TOO!";
+contentTwo.appendChild(headOne);
+contentTwo.appendChild(paraTwo);
+container.appendChild(contentTwo);
+
